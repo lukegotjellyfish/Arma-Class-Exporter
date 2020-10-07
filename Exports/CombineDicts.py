07ccd311cb731175315d33e9bd13561d98fc6dc7
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-10-07 21:09:01 +0100

    Update CombineDicts.py

--- Exports/CombineDicts.py
+++ Exports/CombineDicts.py
@@ -1,6 +1,23 @@
 import os
 
 
+def writeToFile(file, root, side, left):
+	print(file)
+	fileContents = '"' + file.replace(".py", "") + '" = {\n'
+	with open(os.path.join(root, file), "r", encoding="latin-1") as catDict:
+		lines = catDict.readlines()
+		for line in lines:
+			if line == lines[0]:
+				fileContents += "    " + line.replace(" =", ":")
+			else:
+				fileContents += "    " + line
+	fileContents += "\n}"
+	if (left == 1):
+		fileContents += ","
+	fileContents += "\n"
+	side.write(fileContents)
+	return
+
 sides = ["BluFor", "OpFor"]
 combineFiles = ["LauncherMagazines", "Launchers", "Magazines", "VehicleMagazines", "Vehicles", "VehicleWeapons", "Weapons"]
 for side in sides:
@@ -12,7 +29,7 @@ for side in sides:
 		cwd = os.getcwd()
 		output = str(cwd) + "\\" + "Exports" + "\\" + side + category + ".py"
 		print("  Output file: " + output)
-		print("  Looking in: " + str(cwd) + side + "\\" + category)
+		print("  Looking in: " + str(cwd) + "\\" + side + "\\" + category)
 		for root, dirs, files in os.walk(cwd + "\\" + side + "\\" + category, topdown = False):
 			joinedFile = ""
 			with open(output, "w", encoding="utf-8") as outFile:
@@ -39,15 +56,24 @@ for side in sides:
 				outFile.write(joinedFile.replace("|", "\\\\"))
 				print("      Written to " + output + "\n")
 
-sideOut = cwd + "\\" + "Exports" + "\\"
-with open(sideOut + "CombinedBluFor.py", "w", encoding="utf-8") as BluFor,  open(sideOut + "CombinedOpFor.py", "w", encoding="utf-8") as OpFor:
+
+# Clear files:
+open("CombinedBluFor.py", 'w').close()
+open("CombinedOpFor.py", 'w').close()
+with open("CombinedBluFor.py", "a", encoding="utf-8") as BluFor,  open("CombinedOpFor.py", "w", encoding="utf-8") as OpFor:
 	for root, dirs, files in os.walk(cwd + "\\" + "Exports", topdown = False):
+		x = 0
 		for file in files:
-			print(file)
-			with open(os.path.join(root, file), "r", encoding="latin-1") as catDict:
-				if file[:3] == "Blu":
-					BluFor.write(catDict.read() + "\n")
-				elif file[:2] == "Op":
-					OpFor.write(catDict.read() + "\n")
+			if file[:2] == files[x][:2]:
+				left = 1
+			else:
+				left = 0
+			if file[:3] == "Blu":
+				writeToFile(file, root, BluFor, left)
+			if file[:2] == "Op":
+				writeToFile(file, root, OpFor, left)
+			x += 1
+
+	BluFor.write
 
 input("HALT")
