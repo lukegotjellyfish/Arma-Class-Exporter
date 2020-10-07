4f3434d941c777543048fbe0533e5f2ef3573f2f
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-10-07 18:30:10 +0100

    Update Script.py

--- UseDicts/Script.py
+++ UseDicts/Script.py
@@ -2,15 +2,27 @@
 import re
 import os
 import csv
-import BluForMagazines
+
+from Exports import BluForLauncherMagazines
+from Exports import BluForLaunchers
+from Exports import BluForMagazines
+from Exports import BluForVehicleMagazines
+from Exports import BluForVehicles
+from Exports import BluForVehicleWeapons
+from Exports import BluForWeapons
+
+from Exports import OpForLauncherMagazines
+from Exports import OpForLaunchers
+from Exports import OpForMagazines
+from Exports import OpForVehicleMagazines
+from Exports import OpForVehicles
+from Exports import OpForVehicleWeapons
+from Exports import OpForWeapons
 
 
 def getAttrib(a, b):
-    try:
-        return BluForMagazines.BluFor_Magazines[a][b]
-    except:
-        print("Failed to find a match")
-        return "failed"
+    return a
 
 
-print(getAttrib("rhs_200rnd_556x45_B_SAW", "author"))
+print(OpForWeapons)
+#print(getAttrib("rhs_200rnd_556x45_B_SAW", "author"))
