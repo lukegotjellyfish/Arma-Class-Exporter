60795151e724d469ee8b581736015f45edfcc52b
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-10-07 21:08:48 +0100

    Update Script.py

--- UseDicts/Script.py
+++ UseDicts/Script.py
@@ -3,21 +3,6 @@ import re
 import os
 import csv
 
-from Exports import BluForLauncherMagazines
-from Exports import BluForLaunchers
-from Exports import BluForMagazines
-from Exports import BluForVehicleMagazines
-from Exports import BluForVehicles
-from Exports import BluForVehicleWeapons
-from Exports import BluForWeapons
-
-from Exports import OpForLauncherMagazines
-from Exports import OpForLaunchers
-from Exports import OpForMagazines
-from Exports import OpForVehicleMagazines
-from Exports import OpForVehicles
-from Exports import OpForVehicleWeapons
-from Exports import OpForWeapons
 
 
 def getAttrib(a, b):
