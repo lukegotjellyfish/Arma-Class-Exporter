f99b1c927e9338636bdd87d02a6abd0a7d84d11c
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-11-26 11:20:27 +0000

    Update ###Export PBOs for viewing###.bat

--- ModPreperation/###Export PBOs for viewing###.bat
+++ ModPreperation/###Export PBOs for viewing###.bat
@@ -5,62 +5,64 @@
 ::
 ::Requres the following on PATH:
 :: PBOConsole (cmdl)  https://github.com/winseros/PBOManager/releases/tag/v0.1.0
-:: CfgConvert         https://community.bistudio.com/wiki/CfgConvert
-:: DeP3               Dhttps://community.bistudio.com/wiki/DeP3d
-:: Pal2PcE            https://community.bistudio.com/wiki/TexView_2
+:: CfgConvert			 https://community.bistudio.com/wiki/CfgConvert
+:: DeP3					 Dhttps://community.bistudio.com/wiki/DeP3d
+:: Pal2PcE				 https://community.bistudio.com/wiki/TexView_2
 
 
 ::Folder to export PBOs to
 SET exportDir=E:\Games\Arma 3 Mod Files
 
 for /r %%I in (*.pbo) do (
-	for /f "delims==" %%F in ("%%I\.\..\..") do (
-	
-		ECHO ====Found pbo in %%~dpnxF====
-		::Create directory for MOD
-		MKDIR "%exportDir%\%%~nF"
-		
-		::If PBO has already been extracted, skip it
-		IF NOT EXIST "%exportDir%\%%~nF\%%~nI" (
-			::Extract PBO contents
-			PBOConsole -unpack "%%I" "%exportDir%\%%~nF\%%~nI"
-			CfgConvert -txt -dst "%exportDir%\%%~nF\%%~nI\config.cpp" "%exportDir%\%%~nF\%%~nI\config.bin"
-			DEL "%exportDir%\%%~nF\%%~nI\config.bin"
-			DEL "%exportDir%\%%~nF\%%~nI\texHeaders.bin"
+	(ECHO "%%~nI" | findstr \V "bin.pbo core.pbo dubbing ui cargoposes signs rocks roads props structures map_ language supplies music plants missions anims animals" 1>NUL) || (
+		for /f "delims==" %%F in ("%%I\.\..\..") do (
 
-			::Change directory to unpacked pbo
-			CD /D "%exportDir%\%%~nF\%%~nI"
-			
-			
-			::Go through all .p3d files in directory
-			FOR /R %%P in (*.p3d) do (
-				::Append file to be processed
-				ECHO %%P>>toRecycle.txt
+			ECHO ====Found pbo in %%~dpnxF====
+			::Create directory for MOD
+			MKDIR "%exportDir%\%%~nF"
+
+			::If PBO has already been extracted, skip it
+			IF NOT EXIST "%exportDir%\%%~nF\%%~nI" (
+				::Extract PBO contents
+				PBOConsole -unpack "%%I" "%exportDir%\%%~nF\%%~nI"
+				CfgConvert -txt -dst "%exportDir%\%%~nF\%%~nI\config.cpp" "%exportDir%\%%~nF\%%~nI\config.bin"
+				DEL "%exportDir%\%%~nF\%%~nI\config.bin"
+				DEL "%exportDir%\%%~nF\%%~nI\texHeaders.bin"
+
+				::Change directory to unpacked pbo
+				CD /D "%exportDir%\%%~nF\%%~nI"
+
+
+				::Go through all .p3d files in directory
+				FOR /R %%P in (*.p3d) do (
+					::Append file to be processed
+					ECHO %%P>>toRecycle.txt
+				)
+				::Go through files to delete processed p3d files
+				for /f "tokens=*" %%a in (toRecycle.txt) do (
+					::Run dep3d on p3d file to convert to mlod
+					DEP3D "%%a"
+					::Recycle processed file
+					DEL "%%a"
+				)
+				::Recycle file of files to recycle
+				DEL toRecycle.txt
+
+
+				::Go through all .paa files in directory
+				for /R %%f in (*.paa) do (
+					Pal2PacE "%%f" "%%~dpnf.png"
+					DEL "%%f"
+				)
+
+
+				::Return to arma directory
+				CD /D %~dp0
 			)
-			::Go through files to delete processed p3d files
-			for /f "tokens=*" %%a in (toRecycle.txt) do (
-				::Run dep3d on p3d file to convert to mlod
-				DEP3D "%%a"
-				::Recycle processed file
-				DEL "%%a"
-			)
-			::Recycle file of files to recycle
-			DEL toRecycle.txt
-			
-			
-			::Go through all .paa files in directory
-			for /R %%f in (*.paa) do ( 
-				Pal2PacE "%%f" "%%~dpnf.png"
-				DEL "%%f"
-			)
-			
-			
-			::Return to arma directory
-			CD /D %~dp0
+			REM IF NOT EXIST <PBO>
 		)
-		REM IF NOT EXIST <PBO>
+		REM for /f "delims==" %%F in ("%%I\.\..\..")
 	)
-	REM for /f "delims==" %%F in ("%%I\.\..\..")
 )
 REM for /r %%I in (*.pbo)
 PAUSE
