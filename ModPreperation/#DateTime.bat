0bd14def32a0cf5eb56e96bc583847d9074641bd
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-11-27 14:27:31 +0000

    Update #DateTime.bat

--- ModPreperation/#DateTime.bat
+++ ModPreperation/#DateTime.bat
@@ -1,5 +1,5 @@
-ECHO        ==================================================
-ECHO        =[DATETIME]   Started at: %1 =
-ECHO        =[DATETIME] Currently at: "%date% %time:~0,-3%" =
-ECHO        ==================================================
+ECHO        =======================================
+ECHO        =   Started at: %1 =
+ECHO        = Currently at: "%date% %time:~0,-3%" =
+ECHO        =======================================
 ECHO.
