0fa2a97759435d1325b83da7d36cb22c31a254db
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-11-27 14:26:18 +0000

    Delete ###LoopThroughPBO###.bat

--- ModPreperation/###LoopThroughPBO###.bat
+++
@@ -1,7 +0,0 @@
-for /r %%i in (*.pbo) do PBOConsole -unpack "%%i" "./%%~ni"
-
-for /r %%I in (config.bin) do (
-	CD %%~pI
-	@IF EXIST %%I CfgConvert -txt -dst "config.cpp" "config.bin"
-	CD %~dp0
-)
