2cd092e5f1d672b32793847ba0432c9d1618d64f
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-11-27 20:56:32 +0000

    Update #DateTime.bat

--- ModPreperation/#DateTime.bat
+++ ModPreperation/#DateTime.bat
@@ -1,12 +1,14 @@
 @echo off
 
+set removeMarx=%1
+set removeMarx=%removeMarx:~1,-1%
 
 IF "%2" EQU "1" (
-ECHO        =======================================
-ECHO        =   Started at: %1 =
-ECHO        = Currently at: "%date% %time:~0,-3%" =
-ECHO        =======================================
-ECHO.
+	ECHO        =====================================
+	ECHO        =   Started at: %removeMarx% =
+	ECHO        = Currently at: %date% %time:~0,-3% =
+	ECHO        =====================================
+	ECHO.
 ) ELSE (
-	ECHO %1 %time:~0,-3%
+	ECHO %removeMarx% [%~z3 Bytes] %time:~0,-3%
 )
