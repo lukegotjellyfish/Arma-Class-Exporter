85c3cb9b5e2235a3b4f640862386c88f7e08381d
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-11-27 17:44:50 +0000

    Update #DateTime.bat

--- ModPreperation/#DateTime.bat
+++ ModPreperation/#DateTime.bat
@@ -1,5 +1,12 @@
+@echo off
+
+
+IF "%2" EQU "1" (
 ECHO        =======================================
 ECHO        =   Started at: %1 =
 ECHO        = Currently at: "%date% %time:~0,-3%" =
 ECHO        =======================================
 ECHO.
+) ELSE (
+	ECHO %1 %time:~0,-3%
+)
