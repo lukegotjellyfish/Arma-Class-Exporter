8f3d9cb957563da2dbf3f6dce942de4b741b2976
lukegotjellyfish <lukegotjellyfish@gmail.com>
2020-11-27 14:26:22 +0000

    Create #DateTime.bat

---
+++ ModPreperation/#DateTime.bat
@@ -0,0 +1,5 @@
+ECHO        ==================================================
+ECHO        =[DATETIME]   Started at: %1 =
+ECHO        =[DATETIME] Currently at: "%date% %time:~0,-3%" =
+ECHO        ==================================================
+ECHO.
