203bdfd78ab0e60c4bc6e022678a5a93745351f0
lukegotjellyfish <lukegotjellyfish@gmail.com>
2021-01-16 02:25:10 +0000

    Create Create folders.bat

---
+++ Exports/Archive RHS/Create folders.bat
@@ -0,0 +1,21 @@
+@echo off
+
+MKDIR RHS\Weapons
+MKDIR RHS\Magazines
+MKDIR RHS\Vehicles
+MKDIR RHS\Glasses
+
+MKDIR Exports
+
+REM CD BluFor
+REM for /r %%i in (*.cpp *.py) do (
+	REM echo Recycled %%~fi
+	REM recycle %%i
+REM )
+REM CD ..\
+REM CD OpFor
+REM for /r %%i in (*.cpp *.py) do (
+	REM echo Recycled %%~fi
+	REM recycle %%i
+REM )
+REM PAUSE
