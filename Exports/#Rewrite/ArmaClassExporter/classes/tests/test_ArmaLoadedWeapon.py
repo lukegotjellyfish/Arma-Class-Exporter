4583d2cde736cbcbe2a04b389563bd035f1d1c5d
GarrusLaptop <lukegotjellyfish@gmail.com>
2024-07-17 01:38:25 +0100

    Remove redundant line

--- Exports/#Rewrite/ArmaClassExporter/classes/tests/test_ArmaLoadedWeapon.py
+++ Exports/#Rewrite/ArmaClassExporter/classes/tests/test_ArmaLoadedWeapon.py
@@ -18,7 +18,5 @@ class TestArma(unittest.TestCase):
         self.assertEqual(arma_instance.get_hit(100), 9.373581678628325, "get_hit should return 9.373581678628325")
         self.assertEqual(arma_instance.get_hit(1000), 2.2208616979412095, "get_hit should return 2.2208616979412095")
 
-        arma_instance.set_magazine()
-
 if __name__ == '__main__':
     unittest.main()
